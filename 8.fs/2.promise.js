bcd4931d4fa3d87fd7b799d3b3767847aee8d045
zhufengzhufeng <894918097@qq.com>
2017-07-08 18:16:05 +0800

    Promise.all

--- 8.fs/2.promise.js
+++ 8.fs/2.promise.js
@@ -1,7 +1,7 @@
 //Promise 承诺  ->  永远没有结果  ->等待 -> 成功 resolve  -> 失败 reject
 //Promise 在node中天生支持 构造函数
 let fs = require('fs');
-function read(url,cb) {
+/*function read(url,cb) {
     fs.readFile(url,'utf8',function (err,data) {
         if(err)console.log(err)
         cb(data);
@@ -9,18 +9,30 @@ function read(url,cb) {
 }
 read('name.txt',function (data) {
     console.log(data);
-});
-
-/*function read(url) {
-    return new Promise(function (resolve,reject) {
+});*/
+function read(url) {
+    return new Promise( (resolve,reject)=> {
         fs.readFile(url,'utf8',function (err,data) {
-            if(err)reject(err);
-            resolve(data)
+            if(err){
+                reject(err);
+            }else{
+                resolve(data)
+            }
         });
     })
 }
-//解决了回调问题
-read('name.txt').then(function (data) {
+//解决了回调问题  then catch resolve reject Promise.all 一起执行
+//在all方法中 都成功 才成功 有一个失败 就失败了
+//[name,age]=['珠峰培训',8]
+Promise.all([read('name.txt'),read('age.txt')]).then(([name,age])=>{
+    let school = {
+        name,age
+    };
+    console.log(school);
+},function (err) {
+    console.log(err);
+})
+/*read('name.txt').then(function (data) {
     console.log(data);
 },function (err) {
     console.log(err);
