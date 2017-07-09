861dbebf7f1cce323b9012da3d20b82e5ec8ba38
zhufengzhufeng <894918097@qq.com>
2017-07-09 16:42:25 +0800

    clock

---
+++ 9.http/clock/server.js
@@ -0,0 +1,28 @@
+let http = require('http');
+let fs = require('fs');
+let mime = require('mime');
+let url = require('url');
+http.createServer(function (req,res) {
+    let {pathname,query} = url.parse(req.url,true);
+
+    if(pathname === '/'){
+        res.setHeader('Content-Type','text/html;charset=utf-8');
+        fs.createReadStream('./index.html').pipe(res);
+    }
+    //1. /clock
+    else if(pathname === '/clock'){
+        let date = new Date().toLocaleString();
+        res.end(JSON.stringify({date})); //string or buffer
+    }
+
+    else{
+        let flag = fs.existsSync('.'+pathname);
+        if(flag){
+            res.setHeader('Content-Type',mime.lookup(pathname)+";charset=utf-8");
+            fs.createReadStream('.'+pathname).pipe(res);
+        }else{
+            res.statusCode = 404;
+            res.end('not Found');
+        }
+    }
+}).listen(5000);
