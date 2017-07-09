0cb7c577c2ba5cf324be185bd5284dc3c5f25a56
zhufengzhufeng <894918097@qq.com>
2017-07-09 10:40:52 +0800

    createStream

--- 8.fs/5.stream.js
+++ 8.fs/5.stream.js
@@ -5,12 +5,20 @@ let fs  = require('fs');
 //3.读取默认都是buffer
 let rs = fs.createReadStream('./name.txt',{highWaterMark:1});
 //默认这种模式 叫 非流动模式 -> 流动模式
-let str = ''; //Buffer.concat
+let arr = []; //Buffer.concat([buf1,buf2,buf3])
 rs.on('data',function (data) { //node会不停的 rs.emit('data',data)
     console.log(data); //疯狂的触发
-    str+=data;
+    arr.push(data);
+    rs.pause(); //暂停的是data事件触发
+    setTimeout(function () {
+        rs.resume();//恢复的是data事件
+    },1000);
 });
 rs.on('end',function () {//当这个文件读取完成后 就会触发end事件
-    console.log(str);
+    console.log(Buffer.concat(arr).toString());
 });
-
+rs.on('error',function (err) { //rs.emit('error',err)
+    console.log(err);
+});
+//on('data') on('end') on('error') pause resume();
+//req是一个可读流
