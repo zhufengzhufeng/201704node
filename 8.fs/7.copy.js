0e8041021acd29bf137f949ba11515c27a53d004
zhufengzhufeng <894918097@qq.com>
2017-07-09 12:03:13 +0800

    fs

--- 8.fs/7.copy.js
+++ 8.fs/7.copy.js
@@ -4,6 +4,7 @@ function copy(source,target) {
     //每次读取一次，一次读取4b,每次写入只能写入1个
     let rs = fs.createReadStream(source,{highWaterMark:4});
     let ws = fs.createWriteStream(target,{highWaterMark:1});
+    //8bit = 1b 3b=1个汉字 1024b = 1k  1024k = 1M  1024M = 1G
     //1.先读一次去写，如果写不下，此时要暂停读取
     rs.on('data',function (data) {
         let flag = ws.write(data);
---
+++ 8.fs/8.pipe.js
@@ -0,0 +1,14 @@
+let fs = require('fs');
+function copy(source,target) {
+    let rs = fs.createReadStream(source,{highWaterMark:4});
+    let ws = fs.createWriteStream(target,{highWaterMark:1});
+    rs.pipe(ws); //可读流的方法pipe 管道  异步，读一点写一点,保证不淹没可用内存
+}
+//什么时候用readFile 什么时候用pipe
+//readFile可以看到读取的内容 pipe 看不到的
+copy('2.txt','4.txt');
+
+let path = require('path');
+//path 内置模块 resolve 解析  join
+console.log(path.resolve('dist','a'));//给相对 会返还给你一个绝对路径
+//console.log(path.join(__dirname,'dist','a'));
